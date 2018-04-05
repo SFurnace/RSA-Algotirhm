62e81777ff938fc4a749da0d2931120668c95136
Zhao Cao <cz188b@foxmail.com>
2018-04-05 09:46:04 +0800

    bug fix: textarea content need to change in gui thread

--- src/app/view/TextController.java
+++ src/app/view/TextController.java
@@ -6,7 +6,6 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TextArea;
 
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -21,18 +20,6 @@ public class TextController {
 
     private Timer timerE;
     private Timer timerD;
-
-    private static String arrayToString(byte[] bytes) {
-        StringBuilder builder = new StringBuilder();
-        for (int i = 0; i < bytes.length; i++) {
-            builder.append(bytes[i]);
-            builder.append(',');
-            if (i % 60 == 59) {
-                builder.append(System.lineSeparator());
-            }
-        }
-        return builder.toString();
-    }
 
     @FXML
     private synchronized void startEncryptText() {
@@ -55,15 +42,15 @@ public class TextController {
 
     private void encryptText() {
         App.getRsa().ifPresentOrElse(
-                rsa -> {
+                rsa -> Platform.runLater(() -> {
                     try {
-                        textInputD.setText(arrayToString(rsa.encryptObj(textInputE.getText())));
+                        textInputD.setText(Arrays.toString(rsa.encryptObj(textInputE.getText())));
                         textInputE.setStyle("text-area-background: rgba(188,255,209,0.5)");
-                    } catch (IOException e) {
+                    } catch (Exception e) {
                         e.printStackTrace();
-                        Platform.runLater(() -> textInputE.setStyle("text-area-background: rgba(255,181,174,0.51)"));
+                        textInputE.setStyle("text-area-background: rgba(255,181,174,0.51)");
                     }
-                },
+                }),
                 () -> Platform.runLater(
                         () -> new Alert(WARNING, "Please choose a RSA key file.").showAndWait()
                 )
@@ -101,13 +88,15 @@ public class TextController {
                         bytes[i] = (byte) integers[i];
                     }
 
-                    try {
-                        textInputE.setText(rsa.decryptObj(bytes));
-                        textInputD.setStyle("text-area-background: rgba(188,255,209,0.51)");
-                    } catch (IOException | ClassNotFoundException e) {
-                        e.printStackTrace();
-                        Platform.runLater(() -> textInputD.setStyle("text-area-background: rgba(255,181,174,0.5)"));
-                    }
+                    Platform.runLater(() -> {
+                        try {
+                            textInputE.setText(rsa.decryptObj(bytes));
+                            textInputD.setStyle("text-area-background: rgba(188,255,209,0.51)");
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            textInputD.setStyle("text-area-background: rgba(255,181,174,0.5)");
+                        }
+                    });
                 },
                 () -> Platform.runLater(
                         () -> new Alert(WARNING, "Please choose a RSA key file.").showAndWait()
